6a695097ce69629457c86bf2b18b426dfffdfdbd
Mateus Leandro <100031728+MateusLSants@users.noreply.github.com>
2023-03-14 10:29:33 -0300

    enabeled database

---
+++ src/main/java/br/com/dev/examgenerator/ExamgeneratorApplication.java
@@ -0,0 +1,13 @@
+package br.com.dev.examgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamgeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamgeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/dev/examgenerator/ExamgeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dev.examgenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamgeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
